6a9911f123705506a87cec5a23a72c49716748d5
Mohammed Khan <mac.3785@gmail.com>
2017-08-08 20:26:54 +0530

    working version 2, fixed cross browser errors in helper.setContractsMap fn.

--- src/aura/SF1_ListContracts_SFDCMAY/SF1_ListContracts_SFDCMAYHelper.js
+++ src/aura/SF1_ListContracts_SFDCMAY/SF1_ListContracts_SFDCMAYHelper.js
@@ -27,14 +27,6 @@
             this.fireContractChangeEvent(null, null, null);
         }
 
-        //console.log('opptyBaslinePicked:' + opptyBaslinePicked + '; acc:' + acc);
-
-        // if (opptyBaslineType == )
-        // var action = component.get("c.getContractsByAccID");
-        // action.setParams({acc:acc});
-        // component.set("v.acc", acc);
-        // selection
-
     },
 
     queryRows : function(component, page, sOrder) {
@@ -75,6 +67,18 @@
                 /* need to uncomment this.
                 */
             }
+            else if (state === "ERROR") {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " + errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
+            } else {
+                console.log("Action State returned was: " + state); 
+            }
 
         });
         $A.enqueueAction(action);
@@ -91,78 +95,47 @@
 
     },   
 
-    setContractsMap : function(component, event) {
+    // setContractsMap1 : function(component, event) {
 
-        var contractList =  component.get("v.contracts");
-        var selectedContractIds = component.get("v.selectedContracts"); 
-        // console.log("contractList=" + contractList);
-        // console.log("selectedContractIds=" + selectedContractIds);
+    //     var contractList =  component.get("v.contracts");
+    //     var selectedContractIds = component.get("v.selectedContracts"); 
+    //     // console.log("contractList=" + contractList);
+    //     // console.log("selectedContractIds=" + selectedContractIds);
 
-        if (!this.isEmpty(contractList) &&  !this.isEmpty(selectedContractIds)) {
+    //     if (!this.isEmpty(contractList) &&  !this.isEmpty(selectedContractIds)) {
 
-            var contractsMap = {};
-            for (var i in contractList){
-                console.log('contract=' + contractList[i]);
-                console.log('contracts.ocd:' + contractList[i].Id);
-                contractsMap[contractList[i].Id]=contractList[i];
-            }
-            console.log('contractsMapsize=' + contractsMap.size + '; contractMap=' + JSON.stringify(contractsMap));
-            component.set("v.contractsMap", contractsMap);
-        }
-    },
+    //         var contractsMap = {};
+    //         for (var i in contractList){
+    //             console.log('contract=' + contractList[i]);
+    //             console.log('contracts.ocd:' + contractList[i].Id);
+    //             contractsMap[contractList[i].Id]=contractList[i];
+    //         }
+    //         console.log('contractsMapsize=' + contractsMap.size + '; contractMap=' + JSON.stringify(contractsMap));
+    //         component.set("v.contractsMap", contractsMap);
+    //     }
+    // },
 
-    getContracts : function(component, event) { 
-        // this.validateEvent(component, event);
-        var acc = event.getParam("acc");
-        //console.log('inside ListContracts.getContacts, acc=' + acc + '; acc.id:' + acc.Id);
-        //var action = component.get("c.getContractsByAccID"); getActiveContractsWithProductsByAccId
-        var action = component.get("c.getActiveContractsWithProductsByAccId"); 
-        action.setParams({acc:acc});
-        component.set("v.acc", acc.Id);
-        
-        action.setCallback(this, function(response) {
-            var state = response.getState();
-            if (component.isValid() && state === "SUCCESS") {
-                component.set("v.contracts", response.getReturnValue());
-                component.set("v.selectedContracts", []);
-                component.set("v.doneLoading", true);
+    setContractsMap : function(component) {
 
-                // start...
-                var contracts = component.get("v.contracts");
-                
-                if((this.isEmpty(contracts)) || (!contracts.length>0)) {
-                    console.log('contracts are empty.');
-                    //component.set("v.doneLoading", false);
-                    component.set("v.noContractsFound", true);
+            var contractList =  component.get("v.contracts");
+            var selectedContractIds = component.get("v.selectedContracts"); 
+            // console.log("contractList=" + contractList);
+            // console.log("selectedContractIds=" + selectedContractIds);
 
-                } else {
-                    console.log('contracts not empty.');
-                    component.set("v.doneLoading", true);
-                    component.set("v.noContractsFound", false);                    
-                }   
-                console.log('contracts=' + contracts);
-                console.log('contracts Length=' + contracts.length);
+            if (!this.isEmpty(contractList) &&  !this.isEmpty(selectedContractIds)) {
 
-                // end...
+                var contractsMap = {};
 
-                //this.hasCurrencyMisMatch(component, event);
-                this.setContractsMap(component, event);
-            }
-            else if (state === "ERROR") {
-                var errors = response.getError();
-                if (errors) {
-                    if (errors[0] && errors[0].message) {
-                        console.log("Error message: " + errors[0].message);
-                    }
-                } else {
-                    console.log("Unknown error");
+                for (var i = 0; i < contractList.length; i++) {
+                    console.log('contract=' + contractList[i]);
+                    console.log('contracts.ocd:' + contractList[i].Id);
+                    contractsMap[contractList[i].Id]=contractList[i];
                 }
-            } else {
-                console.log("Action State returned was: " + state); 
+                console.log('contractsMapsize=' + contractsMap.size + '; contractMap=' + JSON.stringify(contractsMap));
+                component.set("v.contractsMap", contractsMap);
             }
-        });
-        $A.enqueueAction(action);
     },
+
     updateAllCheckboxeshelper: function(component, event, helper) {
         var checked = event.target.checked; //event.target.id event.target.name
         var inputs = document.querySelectorAll("input[type='checkbox']");
@@ -171,11 +144,8 @@
         }
     },
 
+    
     fireContractChangeEvent : function (acc, selectedContracts, currencyMisMatch) {
-
-
-        // var currencyMisMatch = component.get("v.isContractCurrencyMismatch");
-        // console.log("fireContractChangeEvent:currencyMisMatch=" + currencyMisMatch);
 
         console.log("fireContractChangeEvent:currencyMisMatch=" + currencyMisMatch + '; selectedContracts=' + selectedContracts + '; acc=' + acc);
 
@@ -196,29 +166,8 @@
         console.log("conMap="+ contractsMap) + '; selectedContracts:' + selectedContractIds;
 
         if (!this.isEmpty(contractsMap) &&  !this.isEmpty(selectedContractIds)) {
-
-            // var contractsMap = {};
-            // for (var i in contractList){
-            //     console.log('contract=' + contractList[i]);
-            //     console.log('contracts.ocd:' + contractList[i].Id);
-            //     contractsMap[contractList[i].Id]=contractList[i];
-            // }
-            // console.log('contractsMapsize=' + contractsMap.size + '; contractMap=' + JSON.stringify(contractsMap));
-            // component.set("v.contractsMap", contractsMap);
             
             var currSet = new Set();
-
-            // for (var i in selectedContractIds) {
-            //     console.log('i=' + i + '; selectedContractIds:' + selectedContractIds[i]);
-            //     var cont= contractsMap[selectedContractIds[i]];
-            //     console.log('currency=' + cont.Currency__c);
-            //     console.log('selectedContrac Rec:' + cont);
-            //     currSet.add(cont.Currency__c);
-            // }
-
-            // console.log('currSet=' + currSet.size + 'currSet=' + currSet.values().next().value);
-
-
             for (var i in selectedContractIds) {
                 console.log('i=' + i + '; selectedContractIds:' + selectedContractIds[i]);
                 var cont= contractsMap[selectedContractIds[i]];
@@ -237,30 +186,7 @@
                 console.log('has No Currency Mis-match');
                 component.set("v.isContractCurrencyMismatch", false);
             }
-
-
-            //console.log('contractsMap size=' + contractsMap.size + '; contractsMap=' + Json.stringify(contractsMap));
-            
-
-            // // loop over contracts and figure out if currency mismatch is there... 
-            // var mySet = new Set();
-
-            // var i = 0;
-            // for (var Original_Contract_Id__c in contractList) {
-            //        if (object.hasOwnProperty(Original_Contract_Id__c)) {
-            //         // do stuff
-            //             console.log('contractID:' + Original_Contract_Id__c + '; contract.currency=' + contractList[i].Currency__c);
-            //         }
-            //         i++;
-            // }
         }
-        // set the isContractCurrencyMismatch field value.
-
-        // var myMap = new Map();
-
-        // myMap.set(keyString, "value associated with 'a string'");
-        // myMap.size;
-        // myMap.get(keyString); 
 
     },
 
