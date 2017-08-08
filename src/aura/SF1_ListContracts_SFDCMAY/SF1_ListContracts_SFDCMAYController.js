61071ea9347554ae797617c426a7dde361554d14
Mohammed Khan <mac.3785@gmail.com>
2017-08-08 18:58:21 +0530

    commiting the working copy of ListContracts

--- src/aura/SF1_ListContracts_SFDCMAY/SF1_ListContracts_SFDCMAYController.js
+++ src/aura/SF1_ListContracts_SFDCMAY/SF1_ListContracts_SFDCMAYController.js
@@ -1,9 +1,9 @@
 ({
-    /*
-     * doInit : function(component, event, helper) {
-        helper.getContracts(component);
-        },
-    */
+	/*
+	 * doInit : function(component, event, helper) {
+		helper.getContracts(component);
+		},
+	*/
     handleRenderContracts : function (component, event, helper) {
         //helper.getContracts(component, event);
         helper.validateEvent(component, event);
@@ -26,54 +26,6 @@
             "slideDevName": 'detail'
         })
         sObjectEvent.fire();
-    }, 
-    pagerecords : function (cmp,event,helper) {
-        if (cmp.isValid()) { 
-            cmp.set("v.page", event.getParam("pagevalue"));
-            helper.queryRows(cmp, cmp.get("v.page") || 0, helper.getSortOrder(cmp, false));
-            // helper.fireContractChangeEvent(null, null, null);
-        }
     },
-    customSort : function(cmp,event,helper) {
-        if (cmp.isValid()) {
-
-            console.log('cmp-->' + JSON.stringify(cmp));
-            var contractIDAPIName = event.getParam("fieldid");
-            cmp.set("v.sortField", contractIDAPIName);
-            cmp.set("v.page", event.getParam("pagevalue"));
-            helper.queryRows(cmp, cmp.get("v.page") || 0, helper.getSortOrder(cmp, true));
-
-            // var currencyMisMatch = component.get("v.isContractCurrencyMismatch");
-            // console.log("mj" + currencyMisMatch); 
-            // helper.fireContractChangeEvent(null, null, null);
-            
-        }
-    },
-    openModal : function(component, event, helper) {
-
-        var contractId = event.target.name;
-        var contractsMap = component.get("v.contractsMap");
-        var contractRec = contractsMap[contractId];
-        //alert('contractRow=' + JSON.stringify(contractRec.Contract_Products__r));
-
-        $A.createComponent(
-            "c:SF1_ViewProducts",
-            // "c:productsViewModal",
-            {
-                // "aura:id": "findableAuraId",
-                "products": contractRec.Contract_Products__r,
-                "headerMsg": contractRec.Original_Contract_Id__c
-             },
-            function(msgBox){               
-                if (component.isValid()) {
-                    var targetCmp = component.find('ModalDialogPlaceholder');
-                    var body = targetCmp.get("v.body");
-                    body.push(msgBox);
-                    targetCmp.set("v.body", body);
-                }
-            }
-        );
-    },
-
     
 })
