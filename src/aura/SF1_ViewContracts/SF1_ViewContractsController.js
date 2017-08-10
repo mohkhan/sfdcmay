c4416280955029044dc0c19d862d137734edc48d
Mohammed Khan <mac.3785@gmail.com>
2017-08-10 14:37:07 +0530

    mob-158 completed. mob-252 as well.

--- src/aura/SF1_ViewContracts/SF1_ViewContractsController.js
+++ src/aura/SF1_ViewContracts/SF1_ViewContractsController.js
@@ -5,5 +5,20 @@
 
 	closeContractModal : function(cmp,event,helper){
         helper.closeModal(cmp);
-    }
+    },
+
+    gotoRecord : function(component,event){
+        console.log('sf1_viewContracts:gotoRec:' + event.target.id);
+        //var monkeyLove = event.getSource().getElement().getAttribute('name');
+        
+        var navigateToId = event.target.id;
+        var sObjectEvent = $A.get("e.force:navigateToSObject");
+        console.log('sObjectEvent=' + sObjectEvent);
+        sObjectEvent.setParams({
+            "recordId": navigateToId,
+            "slideDevName": 'detail'
+        })
+        sObjectEvent.fire();
+    },
+
 })
